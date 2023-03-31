d629f378e07d2b0be158b952450a750d146dc2d9
Ali-Assaad <aliassaadtr@gmail.com>
2023-03-31 19:46:15 +0300

    Create Navbar component with logo and navigation links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import "./navbar.scss";
 
 const Navbar = () => {
   return (
-    <div>
-        Navbar
+    <div class="navbar">
+        <div className="container">
+            <div className="left">
+                <img src="https://commons.wikimedia.org/wiki/File:Netflix_2015_logo.svg#/media/File:Netflix_logo.svg" alt="logo img" class="logo" />
+                <span>Homepage</span>
+                <span>Series</span>
+                <span>Movies</span>
+                <span>New and Popular</span>
+                <span>My List</span>
+            </div>
+        </div>
     </div>
   )
 }

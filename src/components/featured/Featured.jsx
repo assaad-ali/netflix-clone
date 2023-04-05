89612daa63c65c9ec06efdb6a38a355321b67c62
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-05 22:52:12 +0300

    feat(Featured): Add img and description

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -9,6 +9,17 @@ function Featured() {
           alt="" 
         />
 
+        <div className="info">
+            <img src="https://media-cache.cinematerial.com/p/500x/jgtvkdgr/hamilton-logo.jpg?v=1592705013" alt="" />
+            <span className="description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Earum incidunt neque nobis sit excepturi ducimus facere 
+                voluptates ab, doloremque fuga aliquid, praesentium optio 
+                eum a consequatur natus ipsam doloribus aperiam!
+            </span>
+            
+        </div>
+
     </div>
   )
 }

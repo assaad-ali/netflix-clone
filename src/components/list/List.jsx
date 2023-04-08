b3e55f75aff155ad98f2e030d45ccfc58c7062eb
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-09 02:23:40 +0300

    feat(list): Add conditional arrow display and update slide handling

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -6,12 +6,19 @@ import { useRef, useState } from 'react';
 
 
 export const List = () => {
-    
+
+    const [isMoved, setIsMoved] = useState(false);
     const[slideNumber, setSlideNumber] = useState(0);
 
     const listRef = useRef()
 
     const handleClick = (direction) => {
+        
+        if(slideNumber === 1 ){
+            console.log(slideNumber)
+            setIsMoved(false);
+        }
+
         let distance = listRef.current.getBoundingClientRect().x - 50;
 
         if(direction === "left" && slideNumber > 0){
@@ -19,6 +26,7 @@ export const List = () => {
             listRef.current.style.transform = `translateX(${distance + 230}px)`
         }
         if(direction === "right" && slideNumber < 5){
+            setIsMoved(true);
             setSlideNumber(slideNumber + 1)
             listRef.current.style.transform = `translateX(${distance - 230}px)`
         }
@@ -28,7 +36,11 @@ export const List = () => {
     <div className='list'>
         <span className="listTitle">Continue to watch</span>
         <div className="wrapper">
-            <ArrowBackIosIcon className='sliderArrow left' onClick= {() => handleClick("left")}/>
+            <ArrowBackIosIcon
+                className='sliderArrow left' 
+                onClick= {() => handleClick("left")}
+                style={{display: !isMoved && 'none'}}
+                />
             <div className="container" ref={listRef}>
                 <ListItem/>
                 <ListItem/>

2512943981d33db1f3dc74a65d21e204cfdeaf28
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-09 02:48:30 +0300

    style(listItem): Update style for listItem component

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -3,7 +3,7 @@ import  './listItem.scss'
 const ListItem = () => {
   return (
     <div className='listItem'>
-        item
+        <img src='https://www.al.com/resizer/c7lh5csiBIzTlZ3TAagpLQHKH4U=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/expo.advance.net/img/6c16adc45f/width2048/ee8_a1.jpeg'/>
     </div>
   )
 }

90a2709125ff5d060c6e7482bbe6437220bb37ba
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-09 04:56:10 +0300

    Add hover effect and additional information to ListItem component

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,27 +1,49 @@
-import  './listItem.scss'
+import './listItem.scss'
 
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import AddIcon from '@mui/icons-material/Add';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import { useState } from 'react';
 
-const ListItem = () => {
-  return (
-    <div className='listItem'>
-        <img src='https://www.al.com/resizer/c7lh5csiBIzTlZ3TAagpLQHKH4U=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/expo.advance.net/img/6c16adc45f/width2048/ee8_a1.jpeg'
-        alt=''
-        />
-        <div className="itemInfo">
-            <div className="icons">
-                <PlayArrowIcon/>
-                <AddIcon/>
-                <ThumbUpIcon/>
-                <ThumbDownIcon/>
+const ListItem = ({index}) => {
+
+    const [isHovered, setIsHovered] = useState(false);
+
+
+    return (
+        <div
+            className='listItem'
+            style={{left: isHovered && index * 225 -50 + index * 2.5}}
+            onMouseEnter={()=> setIsHovered(true)}
+            onMouseLeave={()=> setIsHovered(false)}
+        >
+            <img src='https://pyxis.nymag.com/v1/imgs/4d1/dcf/a33209cb40d7dbd980ba6cdceddc56b0f6-got-poster-s8-2.2x.rhorizontal.w700.jpg'
+                alt=''
+            />
+            <div className="itemInfo">
+                <div className="icons">
+                    <PlayArrowIcon />
+                    <AddIcon />
+                    <ThumbUpIcon />
+                    <ThumbDownIcon />
+                </div>
+                <div className="itemInfoTop">
+                    <span>1hour 40mins</span>
+                    <span className="age">+16</span>
+                    <span>2001</span>
+
+                </div>
+                <div className="description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Enim mollitia eius cum rerum, ducimus ad delectus in,
+                    doloremque officiis soluta ratione.
+                </div>
+                <div className="genre">Thriller</div>
             </div>
         </div>
-    </div>
-    
-  )
+
+    )
 }
 
 export default ListItem

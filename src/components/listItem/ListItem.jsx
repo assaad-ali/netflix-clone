ad222f983a5496a03eab2ad4d1550e346f135bbe
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-09 03:47:48 +0300

    Add item info icons to ListItem component

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,10 +1,26 @@
 import  './listItem.scss'
 
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+
 const ListItem = () => {
   return (
     <div className='listItem'>
-        <img src='https://www.al.com/resizer/c7lh5csiBIzTlZ3TAagpLQHKH4U=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/expo.advance.net/img/6c16adc45f/width2048/ee8_a1.jpeg'/>
+        <img src='https://www.al.com/resizer/c7lh5csiBIzTlZ3TAagpLQHKH4U=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/expo.advance.net/img/6c16adc45f/width2048/ee8_a1.jpeg'
+        alt=''
+        />
+        <div className="itemInfo">
+            <div className="icons">
+                <PlayArrowIcon/>
+                <AddIcon/>
+                <ThumbUpIcon/>
+                <ThumbDownIcon/>
+            </div>
+        </div>
     </div>
+    
   )
 }
 
